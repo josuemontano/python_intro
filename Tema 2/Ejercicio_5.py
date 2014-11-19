589b3b013c67df79c7a4779f7057499291b07e28
Josue Montano <josuemontanoa@gmail.com>
2014-11-19 00:09:56 -0400

    Updated .gitignore, removed idea project

--- Tema 2/Ejercicio_5.py
+++ Tema 2/Ejercicio_5.py
@@ -5,7 +5,7 @@
 
 
 def suma_matricial(matriz_A, matriz_B):
-    """ Funcion que, dadas las matrices A y B, devuelve la matriz A + B """
+    """ Función que, dadas las matrices A y B, devuelve la matriz A + B """
     if verificar_matrices_para_suma(matriz_A, matriz_B):
         # Construimos una matriz de la misma que la matriz B
         ans = [[0 for i in range(len(matriz_B[0]))] for j in range(len(matriz_B))]
@@ -18,7 +18,7 @@ def suma_matricial(matriz_A, matriz_B):
 
 
 def verificar_matrices_para_suma(matriz_A, matriz_B):
-    """ Funcion que verifica si las matrices dadas pueden ser sumadas """
+    """ Verifica si las matrices dadas pueden ser sumadas """
     return (len(matriz_A) == len(matriz_B)) and (len(matriz_A[0]) == len(matriz_B[0]))
 
 matriz_A = [[1, 1],
