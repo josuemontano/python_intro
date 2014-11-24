24bf63e95a06aa00b3741ebfd87d389dc3057602
Josue Montano <josuemontanoa@gmail.com>
2014-11-24 12:25:53 -0400

    cmath new lesson

---
+++ tema_1/Leccion_15.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Programación interactiva en Python
+# Lección 15
+# Módulo cmath
+
+from cmath import *
+
+
+x = complex(-1.0, 0.0)
+print(x)
+
+print(x.real)
+print(x.imag)
+
+print(phase(x))
+print(polar(x))
+print(rect(e, 0))
+
+print(exp(x))
+print(sqrt(x))
+
+print(abs(x))
