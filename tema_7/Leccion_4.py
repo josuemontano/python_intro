d24c4038738c43e15853dd8f999ce580011034a5
Josue Montano <josuemontanoa@gmail.com>
2015-01-21 15:41:46 -0400

    Update Leccion_4.py

--- tema_7/Leccion_4.py
+++ tema_7/Leccion_4.py
@@ -7,11 +7,8 @@ import os
 
 
 directorio = os.getcwd()
-archivo = open(directorio + os.sep + "Lorem_ipsum.txt", "w")
-
-archivo.write("LOREM IPSUM")
-archivo.write("\n")
-archivo.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae metus quis est feugiat mollis. ")
-archivo.write("Aenean tincidunt justo rutrum, ornare odio at, placerat purus. Sed laoreet non libero eget malesuada.")
-
-archivo.close()
+with open(directorio + os.sep + "Lorem_ipsum.txt", "w") as archivo:
+    archivo.write("LOREM IPSUM")
+    archivo.write("\n")
+    archivo.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae metus quis est feugiat mollis. ")
+    archivo.write("Aenean tincidunt justo rutrum, ornare odio at, placerat purus. Sed laoreet non libero eget malesuada.")
